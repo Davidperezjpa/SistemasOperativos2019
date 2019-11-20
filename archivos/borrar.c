917e25670bc42177b03820c77ba3ab2689d3b7cb
David Javier Perez Alvarez <davidperezjpa@gmail.com>
2019-11-20 00:09:49 +0000

    Practica 9

---
+++ archivos/borrar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+    char *file = argv[1];
+    char *pwd = getcwd(NULL,0);
+    char path[strlen(file) + strlen(pwd) + 2];
+    sprintf(path, "%s/%s",pwd,file);
+    printf("pwd %s\n", path);
+    
+    printf("%d",unlink(path));
+} 
